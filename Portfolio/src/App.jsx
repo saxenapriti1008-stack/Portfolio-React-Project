f3700c707a58b47337e3f9be8594bf16220d6f63
Priti Saxena <saxenapriti1008@gmail.com>
2025-11-15 23:17:06 +0100

    updated app.jsx file
    updated app.jsx.file

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import Projects from "./component/project.jsx";
+import Projects from "..src/component/project.jsx";
 
 import productlyImg from "./assets/image1.png";
 import genesisImg from "./assets/image2.png";
