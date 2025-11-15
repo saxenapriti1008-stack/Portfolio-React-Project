352960924d5a79e2087aa1d0f1e73d4755d96354
Priti Saxena <saxenapriti1008@gmail.com>
2025-11-15 15:56:35 +0100

    Added profile pic
    added portfolio.jsx file

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import Portfolio from "./components/portfolio.jsx";
+
+
+function App() {
+  
+return (
+  <>
+  <Portfolio />
+  </>
+)
+  
+}
+
+export default App;
---
+++ Portfolio/src/components/portfolio.jsx
@@ -0,0 +1,17 @@
+import portfolioPic from "../assets/profile.png";
+function Portfolio () {
+    return (
+      <>
+      <img src={portfolioPic} alt="Profile Picture" />
+      <h2>Priti</h2>
+      <p>As a Frontend Developer, you're learning how to turn ideas into interactive, visually appealing websites using HTML, CSS, JavaScript, and frameworks like React. You're building the foundation for creating reusable components, managing state, and making user interfaces that feel smooth and intuitive. Every challenge you face now is part of the normal learning curve, and each step strengthens your problem-solving and technical confidence. Keep going — you’re developing real, practical skills that will only continue to grow from here.</p>
+      
+      
+      </>
+      
+
+      
+    )
+}
+
+export default Portfolio;
